a7f89c90b28fafdbbe59771965d5f6b7798f6433
Kelby Hubbard <kelby@pop-os.localdomain>
2021-01-17 12:42:42 -0900

    Kelby Hubbard added.

--- main.cpp
+++ main.cpp
@@ -2,5 +2,6 @@
 using std::cout;
 int main() {
    cout << "Professor Hartman doesn't need a grade, he's the professor.\n";
+   cout << "Kelby Hubbard should receive a 100 on HW1.\n";
    return 0;
 }
