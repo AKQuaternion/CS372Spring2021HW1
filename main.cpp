e47bbd003509aef8525a1b956567eca24c2daa95
Lawtonlegacy <rjlawton@alaska.edu>
2021-01-20 15:13:01 -0800

    Robert Lawton added a line.

--- main.cpp
+++ main.cpp
@@ -4,5 +4,6 @@ int main() {
    cout << "Professor Hartman doesn't need a grade, he's the professor.\n";
    cout << "Kelby Hubbard should receive a 100 on HW1.\n";
    cout << "Rose Peters should get a 20/20 on homework 1.\n";
+   cout << "Robert Lawton should recieve a 100 on HW1.\n";
    return 0;
 }
