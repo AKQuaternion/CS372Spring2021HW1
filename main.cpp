a5e64851e676e405a865a452526d9483e0be9243
Rose Peters <rmpeters3@alaska.edu>
2021-01-20 13:26:37 -0900

    Rose Peters added a line

--- main.cpp
+++ main.cpp
@@ -3,5 +3,6 @@ using std::cout;
 int main() {
    cout << "Professor Hartman doesn't need a grade, he's the professor.\n";
    cout << "Kelby Hubbard should receive a 100 on HW1.\n";
+   cout << "Rose Peters should get a 20/20 on homework 1.\n";
    return 0;
 }
