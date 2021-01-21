3664c238222d726262b261bf2e1bf5eed1d987b6
Jacob Jakiemiec <jrjakiemiec@alaska.edu>
2021-01-20 15:16:38 -0900

    Add the score for Jacob

--- main.cpp
+++ main.cpp
@@ -5,5 +5,6 @@ int main() {
    cout << "Kelby Hubbard should receive a 100 on HW1.\n";
    cout << "Rose Peters should get a 20/20 on homework 1.\n";
    cout << "Robert Lawton should recieve a 100 on HW1.\n";
+   cout << "Jacob Jakiemiec should receive a 100 on HW 1.\n";
    return 0;
 }
